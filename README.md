d60959480e4ca907ffff69d57015e84c7937fd7f
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-12-05 22:22:23 +0530

    Navbar Added

---
+++ README.md
@@ -0,0 +1,9 @@
+>> React -> npm create vite@latest
+    -- Remove App.css, assets from src
+    -- Modify App.jsx (keeping or rendering all components there)
+
+>> TailwindCSS -> npm install tailwindcss @tailwindcss/vite
+
+>> src -> Components -> All Components
+
+>> src -> Constants -> index.js -> all data(json)
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { navIcons, navLinks } from '@constants'
+import dayjs from 'dayjs'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className='font-bold'>Deeksha's Portfolio</p>
+
+        <ul>
+            {navLinks.map(({ id, name}) => (
+                <li key={id} className='font-medium'>
+                    <p>{name}</p>
+                </li>
+            ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+            {navIcons.map(({ id, img}) => (
+                <li key={id}>
+                    <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                </li>
+            ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM h:mm A")}</time>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
